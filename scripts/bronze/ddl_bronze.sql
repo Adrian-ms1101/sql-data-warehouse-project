c063069988527fe2b2b9a0e97db4667359ab7446
Adrian Martinez <andrew1998654@gmail.com>
2026-05-26 08:12:17 -0700

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,10 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+	This script creates tables in the 'bronze' schema, dropping existing tablas 
+	if the already exist.
+	Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
